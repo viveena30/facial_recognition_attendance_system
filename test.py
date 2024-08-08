a04560f98d6ec53531b3552ead8a05eb4c0ab0e2
viveena rathi <viveena30@gmail.com>
2024-08-08 23:24:45 +0530

    take attendance

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ with open('data/names.pkl', 'rb') as w:
 with open('data/faces_data.pkl', 'rb') as f:
     FACES=pickle.load(f)
 
-# print('Shape of Faces matrix --> ', FACES.shape)
+print('Shape of Faces matrix --> ', FACES.shape)
 
 knn=KNeighborsClassifier(n_neighbors=5)
 knn.fit(FACES, LABELS)
